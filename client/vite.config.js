c2495c663177a97008b2aa7e59ebce6c48f95f32
Marvin <marvin.zhai@sjsu.edu>
2024-12-03 14:14:38 -0800

    edit vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
+
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],[svgr()],
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
   },
-  
 });
