300eec58f9a6bffaf59a3b597decb8ae93a51070
xusamson8 <xusamson14@gmail.com>
2024-12-05 02:13:59 -0800

    users on post

--- client/src/pages/ActivityWall.jsx
+++ client/src/pages/ActivityWall.jsx
@@ -122,33 +122,6 @@ function ActivityWall() {
 
   // return (
   //   <div className="space-y-4">
-  //     <div className="bg-white rounded-lg shadow-md p-4">
-  //       <div className="flex items-center space-x-4">
-  //         <img src={profilePic} alt="User" className="w-12 h-12 rounded-full" />
-  //         <input
-  //           type="text"
-  //           placeholder="Start a post"
-  //           value={newPostContent}
-  //           onChange={(e) => setNewPostContent(e.target.value)}
-  //           className="flex-grow bg-gray-100 rounded-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-  //         />
-  //         <button
-  //           onClick={handleCreatePost}
-  //           className="bg-blue-500 text-white rounded-full px-4 py-2 hover:bg-blue-600"
-  //         >
-  //           Post
-  //         </button>
-  //       </div>
-  //     </div>
-  //     <div className="bg-white rounded-lg shadow-md p-4">
-  //       <input
-  //         type="text"
-  //         placeholder="Search by title"
-  //         value={searchQuery}
-  //         onChange={(e) => setSearchQuery(e.target.value)}
-  //         className="w-full bg-gray-100 rounded-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-  //       />
-  //     </div>
   //     {filteredActivities.map((activity) => (
   //       <div key={activity.id} className="bg-white rounded-lg shadow-md p-4">
   //         <div className="flex items-center space-x-4">
@@ -240,9 +213,13 @@ function ActivityWall() {
             className="bg-[#fbfbf9] rounded-lg shadow-md p-4 mt-4"
           >
             <h3 className="font-semibold text-xl">{post.title}</h3>
-            <p className="text-gray-500 mt-2">{post.text}</p>
+            <p className="text-gray-500 mt-2">Author: {post.creator}</p>
+            <p className="text-gray-400 mt-2">{post.text}</p>
+            
           </div>
         ))}
+
+
       </div>
     </div>
   );
